1ea518af3358d2d8525f5f9bcd5674bacb69d878
Hanshita <hanshitapoplani26@gmail.com>
2025-09-10 22:05:48 +0530

    user and video modal

---
+++ src/models/user.modal.js
@@ -0,0 +1,129 @@
+import mongoose , {Schema} from 'mongoose'; 
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const userSchema = new mongoose.Schema(
+    {
+    watchHistory:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video",
+    }],
+        username:{
+            type:String,
+            required:true,
+            lowercase:true,
+            unique:true,
+            trim:true,
+            index:true, // searchbale optimisation
+        },
+        email:{
+            type:String,
+            required:true,
+            lowercase:true,
+            unique:true,
+            trim:true,
+
+        },
+        fullName:{
+            type:String,
+            required:true,
+            lowercase:true,
+            unique:true,
+            index:true,
+
+        },
+        avatar:{
+            type:String, 
+            required:true,// cloudinary string will be used
+
+        },
+        coverImage:{
+            type:String,
+        },
+        password:{
+            type:String,
+            required:[true,"password id required"],
+            validate:{
+                validator:function(value){
+                    return /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+                },
+                message:"Enter a valid password",
+            }
+        },
+        refreshToken:{
+            type:String,
+            required:true,
+        }
+
+    },
+    
+    {timestamps:true});
+
+
+    // pre hook:-
+    // dont use arrow function , because there we cannot use this keyword , which we highly need here
+userSchema.pre("save",async function (next){
+
+    if(!this.isModified("password")){
+        return next();
+    }// so that it dont encrypt again and again on every reloading
+
+    this.password= bcrypt.hash(this.password,10);
+    next();
+    // this takes 2 fields - kis chiz ko hash karna hai and how many rounds of hashing
+
+
+});
+
+// custom methods can also be created using methods.
+// we are comparing passwords
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password,this.password);
+    // returns in true and false
+}
+
+
+
+userSchema.methods.generateAccessToToken= function(){
+
+    jwt.sign(
+        {
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName,
+
+    },
+    process.env.ACCESS_TOKEN_SECRETKEY,
+    {
+        expiresIn:process.env.ACESS_TOKEN_EXPIRY,
+    }
+
+)
+
+}
+
+userSchema.methods.generateRefreshToken= function(){
+
+    jwt.sign(
+        {
+            // less info
+            _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+)
+    
+}
+
+export const User=mongoose.model("User",userSchema);
+
+// token - jwt (json web token)
+// bcrypt - for hashing of password
+
+// so jsonwebtoken cannot be applied directly , we need to use mongoose hooks - 
+// one of them is PRE - ki for instance user wants to save password , so if we  apply
+// pre hook , just befire saving, it will do something - maybe hash the password
---
+++ src/models/video.modal.js
@@ -0,0 +1,66 @@
+
+import mongoose from 'mongoose';
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+;
+
+const videoSchema=new mongoose.Schema(
+    {
+        id:{
+            type:Number,
+            unique:true,
+        },
+        videoFile:{
+            type:String, // cloudinary url
+            required:true,
+
+        },
+        owner:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
+
+        },
+        title:{
+            type:Number,
+            required:true,
+            unique:true,
+
+        },
+        description:{
+            type:String,
+            required:true,
+
+
+        },
+        thumbnail:{  // cloudinary url
+            type:String,
+            required:true,
+
+
+        },
+        duration:{  // cloudinary url  
+            type:Number,
+            required:true,
+
+        },
+        views:{
+            type:Number,
+            required:true,
+
+        },
+        isPublished:{
+            type:boolean,
+            required:true,
+
+        },
+
+
+
+    },
+    
+    {timestamps:true});
+
+
+videoSchema.plugin(mongooseAggregatePaginate) // we can aggregate pipelines through this
+
+
+export const Video=mongoose.model("Video",videoSchema);
